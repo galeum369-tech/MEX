f4266412d2274380039ae09e504d2c914f696a33
galeum369-tech <galeum369@gmail.com>
2026-01-28 00:38:58 +0900

    인풋

--- Assets/_Script/Player/PlayerInputHandler.cs
+++ Assets/_Script/Player/PlayerInputHandler.cs
@@ -12,13 +12,64 @@ public class PlayerInputHandler : MonoBehaviour
     public event Action OnHeal;            // E
     public event Action OnInteract;        // F
 
-    InputSystem_Actions input;
+    PlayerCon input;
 
     private void Awake()
     {
-        input = new InputSystem_Actions();
+        input = new PlayerCon();
 
-        //input.Player.Move.performed += HandleMove;
-
+        input.Player.Move.performed += HandleMove;
+        input.Player.Move.canceled += HandleMove;
+        input.Player.Attack.performed += HandleAttack;
+        input.Player.SupportSkill.performed += HandleSupportSkill;
+        input.Player.Dash.performed += HandleDash;
+        input.Player.Dash.canceled += HandleDash;
+        input.Player.Dodge.performed += HandleDodge;
+        input.Player.Heal.performed += HandleHeal;
+        input.Player.Interact.performed += HandleInteract;
     }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+    private void OnDestroy()
+    {
+        input?.Dispose();
+    }
+
+    #region 입력 이벤트 핸들러
+    void HandleMove(InputAction.CallbackContext context)
+    {
+        OnMove?.Invoke(context.ReadValue<Vector2>());
+    }
+    void HandleAttack(InputAction.CallbackContext context)
+    {
+        OnAttack?.Invoke();
+    }
+    void HandleSupportSkill(InputAction.CallbackContext context)
+    {
+        OnSupportSkill?.Invoke();
+    }
+    void HandleDash(InputAction.CallbackContext context)
+    {
+        OnDash?.Invoke();
+    }
+    void HandleDodge(InputAction.CallbackContext context)
+    {
+        OnDodge?.Invoke();
+    }
+    void HandleHeal(InputAction.CallbackContext context)
+    {
+        OnHeal?.Invoke();
+    }
+    void HandleInteract(InputAction.CallbackContext context)
+    {
+        OnInteract?.Invoke();
+    }
+    #endregion
 }
--- Assets/수업때 한 참고할 스크립트/InputHandler.cs
+++ Assets/수업때 한 참고할 스크립트/InputHandler.cs
@@ -1,50 +1,50 @@
-using UnityEngine;
-using System;
-using UnityEngine.InputSystem;
+//using UnityEngine;
+//using System;
+//using UnityEngine.InputSystem;
 
-public class InputHandler : MonoBehaviour
-{
-    //이벤트 정의
-    public event Action<Vector2> OnMove;
-    public event Action OnAttack;
+//public class InputHandler : MonoBehaviour
+//{
+//    //이벤트 정의
+//    public event Action<Vector2> OnMove;
+//    public event Action OnAttack;
 
-    //입력 액션 참조
-    InputSystem_Actions input;
+//    //입력 액션 참조
+//    InputSystem_Actions input;
 
-    private void Awake()
-    {
-        input = new InputSystem_Actions();
+//    private void Awake()
+//    {
+//        input = new InputSystem_Actions();
 
-        input.Player.Move.performed += HandleMove;
-        input.Player.Move.canceled += HandleMove;
-        input.Player.Attack.performed += HandleAttack;
-    }
+//        input.Player.Move.performed += HandleMove;
+//        input.Player.Move.canceled += HandleMove;
+//        input.Player.Attack.performed += HandleAttack;
+//    }
 
     
 
-    private void OnEnable()
-    {
-        input.Enable();
-    }
-    private void OnDisable()
-    {
-        input.Disable();
-    }
+//    private void OnEnable()
+//    {
+//        input.Enable();
+//    }
+//    private void OnDisable()
+//    {
+//        input.Disable();
+//    }
 
-    private void OnDestroy()
-    {
-        input?.Dispose();
-    }
+//    private void OnDestroy()
+//    {
+//        input?.Dispose();
+//    }
 
-    #region 입력 이벤트 핸들러
-    void HandleMove(InputAction.CallbackContext context)
-    {
-        OnMove?.Invoke(context.ReadValue<Vector2>());
-    }
+//    #region 입력 이벤트 핸들러
+//    void HandleMove(InputAction.CallbackContext context)
+//    {
+//        OnMove?.Invoke(context.ReadValue<Vector2>());
+//    }
 
-    void HandleAttack(InputAction.CallbackContext context)
-    {
-        OnAttack?.Invoke();
-    }
-    #endregion
-}
+//    void HandleAttack(InputAction.CallbackContext context)
+//    {
+//        OnAttack?.Invoke();
+//    }
+//    #endregion
+//}
